373422a4419628fa8f7f5733098621ecb36beaf6
Andrew Johnson <andrewjohnso17@gmail.com>
2024-12-09 14:31:52 -0800

    added the initial country container

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from './components/Header'
+import CountryContainer from './CountryContainer'
 function App() {
 
   return (
     <div className='main'>
       <Header/>
+      <CountryContainer/>
     </div>
   )
 }
---
+++ src/CountryContainer.tsx
@@ -0,0 +1,10 @@
+import './CountryContainer.css'
+
+
+function CountryContainer() {
+    return (
+        <div className="country-container">Countries go here</div>
+    )
+}
+
+export default CountryContainer
