a6a34fe0a4a9dc645c6cfcebba7adc5a15cfcef6
Andrew Johnson <andrewjohnso17@gmail.com>
2024-12-10 13:13:27 -0800

    initial config for SearchBar

--- src/CountryContainer.tsx
+++ src/CountryContainer.tsx
@@ -1,9 +1,10 @@
 import './CountryContainer.css'
+import SearchBar from './SearchBar'
 
 
 function CountryContainer() {
     return (
-        <div className="country-container">Countries go here</div>
+        <div className="country-container"><SearchBar/></div>
     )
 }
 
---
+++ src/SearchBar.tsx
@@ -0,0 +1,9 @@
+import './SearchBar.css'
+
+function SearchBar() {
+    return (
+        <div className="search-bar-container">SearchBrr</div>
+    )
+}
+
+export default SearchBar
