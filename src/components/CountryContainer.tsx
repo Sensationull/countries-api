c19b45376746d37a04ee1b7f843ef397811db172
Andrew Johnson <andrewjohnso17@gmail.com>
2024-12-23 13:02:04 -0800

    initial search for countries implemented with errors stored but not displayed

--- src/components/CountryContainer.tsx
+++ src/components/CountryContainer.tsx
@@ -1,7 +1,32 @@
-import { useEffect, useState } from 'react'
+import { ChangeEvent, useEffect, useState } from 'react'
 import CountryCard from './CountryCard'
 import './CountryContainer.css'
 import SearchBar from './SearchBar'
+
+type CountryInfo = {
+  flags: {
+    alt: string
+    png: string
+    svg: string
+  }
+  name: {
+    common: string
+    nativeName: {
+      eng: string
+      common: string
+    }
+    official: string
+  }
+  population: string
+  region: string
+  capital: string
+}
+
+type CountryData = {
+  data: CountryInfo[] | []
+  isLoading: boolean
+  error: { status: number; message: string } | null
+}
 
 function CountryContainer() {
   /*
@@ -11,51 +36,86 @@ function CountryContainer() {
 
     next 
     4. set up Prettier/ES lint to format on save Done
-    5. figure out what to do for overflow
-    6. set up filter button position on small screens
     7. Search functionality
+        a. Create controlled react input field Done
+        b. fetch according countries Done
+        c. handle error states ~ current
+            1. error states now configured, display error later
+        d. debouncing
     8. Filter functionality
+    6. set up filter button position on small screens
+    5. figure out what to do for overflow
     9. Filter button styling
     */
-  const [countries, setCountries] = useState<CountryInfo[] | []>([])
-  useEffect(() => {
-    fetch(
+  const [value, setValue] = useState('')
+  const [countryData, setCountryData] = useState<CountryData>({
+    data: [],
+    isLoading: false,
+    error: null,
+  })
+
+  const fetchInitialCountries = async () => {
+    setCountryData({ data: [], isLoading: true, error: null })
+    await fetch(
       'https://restcountries.com/v3.1/all?fields=name,flags,capital,region,population'
     )
       .then((response) => response.json())
-      .then((data) => setCountries([...data]))
-      .catch((error) => console.log(error))
+      .then((data) =>
+        setCountryData({ data: [...data], isLoading: false, error: null })
+      )
+      .catch((error) => {
+        setCountryData({
+          data: [],
+          isLoading: false,
+          error: { status: error.status, message: error.message },
+        })
+        console.log(error)
+      })
+  }
+
+  const fetchSpecificCountries = async () => {
+    setCountryData({
+      data: [...countryData.data],
+      isLoading: true,
+      error: null,
+    })
+    const response = await fetch(
+      `https://restcountries.com/v3.1/name/${value}?fields=name,flags,capital,region,population`
+    )
+    if (!response.ok) {
+      const error = await response.text()
+      const parsedError = JSON.parse(error)
+      setCountryData({
+        data: [...countryData.data],
+        isLoading: false,
+        error: { status: parsedError.status, message: parsedError.message },
+      })
+      throw new Error(error)
+    }
+    const data = await response.json()
+    setCountryData({ data: [...data], isLoading: false, error: null })
+  }
+
+  useEffect(() => {
+    fetchInitialCountries()
   }, [])
 
   useEffect(() => {
-    console.log({ countries })
-  }, [countries])
+    if (value) {
+      fetchSpecificCountries()
+    }
+  }, [value])
 
-  type CountryInfo = {
-    flags: {
-      alt: string
-      png: string
-      svg: string
-    }
-    name: {
-      common: string
-      nativeName: {
-        eng: string
-        common: string
-      }
-      official: string
-    }
-    population: string
-    region: string
-    capital: string
+  const handleSearch = (event: ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value)
   }
 
   return (
     <main className="country-container">
-      <SearchBar />
+      <SearchBar value={value} handleSearch={handleSearch} />
       <div className="country-card-container">
-        {countries &&
-          countries.map((country) => {
+        {countryData.data &&
+          countryData.data.map((country) => {
             const { flags, name, population, region, capital } = country
             return (
               <CountryCard
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,17 +1,27 @@
+import { ChangeEvent } from 'react'
 import Filter from './Filter'
 import Search from './Search'
 import './SearchBar.css'
 
-function SearchBar() {
-    return (
-        <div className="search-bar-container">
-            <div className='search-bar'>
-                <Search/>
-                <input placeholder="Search for a country..."/>
-            </div>
-            <Filter/>
-        </div>
-    )
+type SearchBar = {
+  value: string
+  handleSearch(event: ChangeEvent<HTMLInputElement>): void
+}
+
+function SearchBar({ value, handleSearch }: SearchBar) {
+  return (
+    <div className="search-bar-container">
+      <div className="search-bar">
+        <Search />
+        <input
+          placeholder="Search for a country..."
+          value={value}
+          onChange={handleSearch}
+        />
+      </div>
+      <Filter />
+    </div>
+  )
 }
 
 export default SearchBar
