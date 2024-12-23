88909c5e874ee7b971734b1b2654fbe6e870d72a
Andrew Johnson <andrewjohnso17@gmail.com>
2024-12-23 11:04:33 -0800

    prettier configging

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -1,37 +1,37 @@
 import './CountryCard.css'
 
 type CountryCardProps = {
-    flags: { alt: string; png: string; svg: string }
-    name: string
-    population: string
-    region: string
-    capital: string
+  flags: { alt: string; png: string; svg: string }
+  name: string
+  population: string
+  region: string
+  capital: string
 }
 
 function CountryCard({
-    flags,
-    name,
-    population,
-    region,
-    capital,
+  flags,
+  name,
+  population,
+  region,
+  capital,
 }: CountryCardProps) {
-    return (
-        <div className="country-card">
-            <img className="country-flag" src={flags.png} alt={flags.alt} />
-            <div className="country-info">
-                <div className="country-name">{name}</div>
-                <div className="country-population">
-                    Population: <span className="card-data">{population}</span>
-                </div>
-                <div className="country-region">
-                    Region: <span className="card-data">{region}</span>
-                </div>
-                <div className="country-capital">
-                    Capital: <span className="card-data">{capital}</span>
-                </div>
-            </div>
+  return (
+    <div className="country-card">
+      <img className="country-flag" src={flags.png} alt={flags.alt} />
+      <div className="country-info">
+        <div className="country-name">{name}</div>
+        <div className="country-population">
+          Population: <span className="card-data">{population}</span>
         </div>
-    )
+        <div className="country-region">
+          Region: <span className="card-data">{region}</span>
+        </div>
+        <div className="country-capital">
+          Capital: <span className="card-data">{capital}</span>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default CountryCard
--- src/components/CountryContainer.tsx
+++ src/components/CountryContainer.tsx
@@ -3,72 +3,74 @@ import CountryCard from './CountryCard'
 import './CountryContainer.css'
 import SearchBar from './SearchBar'
 
-
 function CountryContainer() {
-    /*
+  /*
     1. Fetch the proper data https://restcountries.com/v3.1/all
     2. Store the data locally
     3. Take the stored data and render it here
 
     next 
-    4. set up Prettier/ES lint to format on save
+    4. set up Prettier/ES lint to format on save Done
     5. figure out what to do for overflow
     6. set up filter button position on small screens
     7. Search functionality
     8. Filter functionality
     9. Filter button styling
     */
-   const [countries, setCountries] = useState<CountryInfo[] | []>([]);
-    useEffect(() => {
-        fetch('https://restcountries.com/v3.1/all?fields=name,flags,capital,region,population')
-        .then((response) => response.json())
-        .then((data) => setCountries([...data]))
-        .catch((error) => console.log(error))
-    },[])
-
-    useEffect(() => {
-        console.log({countries})
-    },[countries]);
-
-    type CountryInfo = {
-        flags: { 
-            alt:string;
-            png: string; 
-            svg: string
-        }
-        name: {
-            common: string;
-            nativeName: {
-                eng: string; 
-                common: string
-            }; 
-            official: string
-        };
-        population: string;
-        region: string;
-        capital: string;
-    }
-
-    return (
-        <main className="country-container">
-            <SearchBar/>
-            <div className='country-card-container'>
-                {countries && countries.map((country) => {
-                    const {flags, name, population, region, capital} = country;
-                    return (
-                    <CountryCard 
-                        flags={flags}
-                        name={name.official}
-                        population={population}
-                        region={region}
-                        capital={capital}
-                        key={name.common}
-                    />
-                )
-                })}
-            </div>
-        </main>
+  const [countries, setCountries] = useState<CountryInfo[] | []>([])
+  useEffect(() => {
+    fetch(
+      'https://restcountries.com/v3.1/all?fields=name,flags,capital,region,population'
     )
+      .then((response) => response.json())
+      .then((data) => setCountries([...data]))
+      .catch((error) => console.log(error))
+  }, [])
+
+  useEffect(() => {
+    console.log({ countries })
+  }, [countries])
+
+  type CountryInfo = {
+    flags: {
+      alt: string
+      png: string
+      svg: string
+    }
+    name: {
+      common: string
+      nativeName: {
+        eng: string
+        common: string
+      }
+      official: string
+    }
+    population: string
+    region: string
+    capital: string
+  }
+
+  return (
+    <main className="country-container">
+      <SearchBar />
+      <div className="country-card-container">
+        {countries &&
+          countries.map((country) => {
+            const { flags, name, population, region, capital } = country
+            return (
+              <CountryCard
+                flags={flags}
+                name={name.official}
+                population={population}
+                region={region}
+                capital={capital}
+                key={name.common}
+              />
+            )
+          })}
+      </div>
+    </main>
+  )
 }
 
 export default CountryContainer
