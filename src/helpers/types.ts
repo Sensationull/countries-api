7ed0934abda6076b3536f261a14943d7297fb1cc
Andrew Johnson <andrewjohnso17@gmail.com>
2025-02-01 12:52:14 -0800

    added types from other files

--- src/helpers/types.ts
+++ src/helpers/types.ts
@@ -22,3 +22,40 @@ export type CountryData = {
   isLoading: boolean
   error: { status: number; message: string } | null
 }
+
+export type FilterProps = {
+  onSelectRegion: (region: string) => void
+}
+
+export type CountryPageInfo = Omit<CountryInfo, 'name'> & {
+  name: {
+    common: string
+    nativeName: {
+      [key: string]: { official: string; common: string }
+    }
+    official: string
+  }
+  subregion: string
+  tld: string[]
+  languages: { [key: string]: string }
+  currencies: { [key: string]: { name: string; symbol: string } }
+  borders: string[]
+}
+
+export type SingleCountryData = {
+  data: CountryPageInfo[] | []
+  isLoading: boolean
+  error: { status: number; message: string } | null
+}
+
+export type CountryCardProps = {
+  flags: { alt: string; png: string; svg: string }
+  name: string
+  population: string
+  region: string
+  capital: string
+}
+
+export type ThemeProviderProps = {
+  children: JSX.Element | JSX.Element[]
+}
